c656badffa77394c865825d4024096974d0c970c
kilyong <uzoolove@gmail.com>
2025-09-25 09:50:36 +0900

    과제 풀이

--- workspace-ins/ch01/ex01-17-02.js
+++ workspace-ins/ch01/ex01-17-02.js
@@ -11,26 +11,88 @@ let grade = '';
 // A: 90점 이상 100점 이하
 if (score >= 90 && score <= 100) {
   grade = 'A';
+} else {
+  // B: 80점 이상 90점 미만
+  if (score >= 80) {
+    grade = 'B';
+  } else {
+    // C: 70점 이상 80점 미만
+    if (score >= 70) {
+      grade = 'C';
+    } else {
+      // D: 60점 이상 70점 미만
+      if (score >= 60) {
+        grade = 'D';
+      } else {
+        // F: 60점 미만
+        grade = 'F';
+      }
+    }
+  }
 }
 
-// B: 80점 이상 90점 미만
-if (score >= 80 && score < 90) {
-  grade = 'B';
+// 소혜림, 박한섭
+if (90 <= score && score <= 100) {
+  grade = 'A';
+} else {
+  if (80 <= score && score < 90) {
+    grade = 'B';
+  } else {
+    if (70 <= score && score < 80) {
+      grade = 'C';
+    } else {
+      if (60 <= score && score < 70) {
+        grade = 'D';
+      } else {
+        grade = 'F';
+      }
+    }
+  }
 }
 
-// C: 70점 이상 80점 미만
-if (score >= 70 && score < 80) {
-  grade = 'C';
-}
-
-// D: 60점 이상 70점 미만
-if (score >= 60 && score < 70) {
-  grade = 'D';
-}
-
-// F: 60점 미만
+// 한정아
 if (score < 60) {
   grade = 'F';
+} else { // 60 이상
+  if (score < 70) {
+    grade = 'D';
+  } else { // 70 이상
+    if (score < 80) {
+      grade = 'C';
+    } else { // 80 이상
+      if (score < 90) {
+        grade = 'B';
+      } else { // 90 이상
+        if (/* score >= 90 && */ score <= 100) {
+          grade = 'A';
+        } else {
+          grade = '재확인 필요한 ';
+        }
+      }
+    }
+  }
+}
+
+// 이승규
+const agradeRange = score >= 90 && score <= 100; // A: 90점 이상 100점 이하
+const bgradeRange = score >= 80 && score < 90; // B: 80점 이상 90점 미만
+const cgradeRange = score >= 70 && score < 80; // C: 70점 이상 80점 미만
+const dgradeRange = score >= 60 && score < 70; // D: 60점 이상 70점 미만
+const fgradeRange = score < 60;                 // F: 60점 미만
+
+if (score != null && typeof score === 'number') {
+    
+    if (agradeRange) grade = 'A';
+    if (bgradeRange) grade = 'B';
+    if (cgradeRange) grade = 'C';
+    if (dgradeRange) grade = 'D';
+    if (fgradeRange) grade = 'F';
+
+    console.log(`${score}점 : ${grade} 학점 입니다.`);
+} else {
+    console.log("score에 0 ~ 100 까지 숫자를 입력해주세요"); 
 }
 
 console.log(`${score}: ${grade}학점`); // 79: C학점
+
+
--- workspace-ins/ch01/ex01-17-03.js
+++ workspace-ins/ch01/ex01-17-03.js
@@ -33,26 +33,119 @@ if (isLogin) {
   if (isUser) console.log('구매자 로그인 상태');
   if (isSeller) console.log('판매자 로그인 상태');
   if (isAdmin) console.log('관리자 로그인 상태');
-}
-
-if (!isLogin) {
+} else {
   if (isUser) console.log('구매자 로그아웃 상태');
   if (isSeller) console.log('판매자 로그아웃 상태');
   if (isAdmin) console.log('관리자 로그아웃 상태');
 }
 
-/* 한정아님 */
-//관리자 로그인상태/관리자 로그아웃상태
-if (isLogin === true && isAdmin === true) {
-  console.log('관리자 로그인상태');
+
+// 소혜림 ////////////////////////
+// 로그인 상태 출력
+if (isLogin) {
+  console.log("로그인 상태");
+} else {
+  console.log("로그아웃 상태");
 }
-if (isLogin === false && isAdmin === true) {
-  console.log('관리자 로그아웃상태');
+// 사용자 유형 출력
+if (isUser) {
+  console.log("구매자");
+} else {
+  if (isSeller) {
+    console.log("판매자");
+  } else {
+    console.log("관리자");
+  }
 }
 
-/* 이승규님 */
-const loginStatus = isLogin ? "로그인 상태" : "로그아웃 상태";
+// 사용자 유형과 로그인 상태를 조합해서 출력
 
-if (isUser)   console.log(`${loginStatus} /구매자 입니다.`);
-if (isSeller) console.log(`${loginStatus} /판매자 입니다.`);
-if (isAdmin)  console.log(`${loginStatus} /관리자 입니다.`);
+if (isUser) { // 구매자
+  if (isLogin) {
+    console.log("구매자 로그인 상태");
+  } else {
+    console.log("구매자 로그아웃 상태");
+  }
+} else {
+  if (isSeller) { // 판매자
+    if (isLogin) {
+      console.log("판매자 로그인 상태");
+    } else {
+      console.log("판매자 로그아웃 상태");
+    }
+  } else { // 관리자
+    if (isLogin) {
+      console.log("관리자 로그인 상태");
+    } else {
+      console.log("관리자 로그아웃 상태");
+    }
+  }
+}
+
+///////////////////////////
+
+// 한정아 //////////////////
+//로그인 상태 출력
+if(isLogin) {
+  console.log('로그인 상태');
+} else { 
+  console.log('로그아웃 상태'); 
+}
+
+//사용자 유형 출력
+if (isUser){
+  console.log('구매자');
+} else {
+  if (isSeller){
+    console.log('판매자');
+  } else {
+    console.log('관리자');
+  }
+}
+
+//로그인상태와 사용자 유형을 조합해서 출력
+if (isUser) {
+  if (isLogin) {
+    console.log('구매자 로그인상태');
+  } else {
+    console.log('구매자 로그아웃상태');
+  }
+}
+
+if (isSeller) {
+  if (isLogin) {
+    console.log('판매자 로그인상태');
+  } else {
+    console.log('판매자 로그아웃상태');
+  }
+}
+
+if (isAdmin) {
+  if (isLogin) {
+    console.log('관리자 로그인상태');
+  } else {
+    console.log('관리자 로그아웃상태');
+  }
+}
+
+///////////////////////////
+
+// 박지은 //////////////////
+let loginStatus = isLogin ? "로그인" : "로그아웃";
+
+// const userType = isUser ? '구매자' : (isSeller ? '판매자' : '관리자');
+// console.log(`${userType} ${loginStatus} 상태`);
+
+if (isUser) {
+  console.log(`구매자 ${loginStatus} 상태`);
+} else {
+  if (isSeller) {
+    console.log(`판매자 ${loginStatus} 상태`);
+  } else {
+    console.log(`관리자 ${loginStatus} 상태`);
+  }
+}
+
+
+
+///////////////////////////
