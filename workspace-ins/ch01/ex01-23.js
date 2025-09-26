4096c8e27dd41618dcab8336616b8781e949e319
kilyong jeong <uzoolove@gmail.com>
2025-09-26 16:05:14 +0900

    new Object() - 생성자 함수로 객체 생성

--- workspace-ins/ch01/ex01-23.js
+++ workspace-ins/ch01/ex01-23.js
@@ -2,5 +2,13 @@
 new Object() - 생성자 함수로 객체 생성
 */
 
+const user = new Object(); // Object 생성자 함수로 객체 생성
 
+// 점 표기법
+user.name = '김하연'; // user 객체에 name 속성을 추가
+user.age = 27; // user 객체에 age 속성을 추가
 
+// 대괄호 표기법
+user['hobby'] = '객체연구'; // user 객체에 hobby 속성을 추가
+
+console.log(user.name, user['age'], user.hobby);
