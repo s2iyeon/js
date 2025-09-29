47b15ae52e3d226803c64bdd81b976a1ca23caeb
kilyong jeong <uzoolove@gmail.com>
2025-09-29 13:50:40 +0900

    객체의 속성을 읽기, 수정, 생성, 삭제하는 방법

--- workspace-ins/ch01/ex01-25.js
+++ workspace-ins/ch01/ex01-25.js
@@ -1,4 +1,21 @@
 /*
 객체의 속성 관리
+객체의 속성을 읽기, 수정, 생성, 삭제하는 방법
 */
+
+const user = {
+  name: '매직핑',
+  mainJob: 'magician',
+};
+
+// 객체의 속성 읽기
+console.log(user.name, user['mainJob'], user.subJob);
+
+// 객체의 속성 수정
+
+// 객체의 속성을 동적으로 생성
+
+// 객체의 속성 삭제
+
+// 객체의 모든 속성 조회
 
