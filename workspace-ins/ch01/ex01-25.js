13f1d0b88dfdde81128f9de8dba0e2f265b0fdfc
kilyong jeong <uzoolove@gmail.com>
2025-09-29 14:31:52 +0900

    객체의 속성을 읽기, 수정, 생성, 삭제하는 방법

--- workspace-ins/ch01/ex01-25.js
+++ workspace-ins/ch01/ex01-25.js
@@ -9,13 +9,23 @@ const user = {
 };
 
 // 객체의 속성 읽기
-console.log(user.name, user['mainJob'], user.subJob);
+console.log(user.name, user['mainJob'].toUpperCase(), user.subJob?.toUpperCase());
 
 // 객체의 속성 수정
+user.mainJob = 'fairy';
+console.log(user.name, user['mainJob'].toUpperCase(), user.subJob?.toUpperCase());
 
 // 객체의 속성을 동적으로 생성
+user.subJob = 'wizard';
+console.log(user.name, user['mainJob'].toUpperCase(), user.subJob?.toUpperCase());
 
 // 객체의 속성 삭제
+// delete user.mainJob;
+console.log(user.name, user['mainJob']?.toUpperCase(), user.subJob?.toUpperCase());
 
 // 객체의 모든 속성 조회
-
+// user 객체의 속성 개수만큼 for 문을 반복한다.
+// prop 변수에는 각 속성의 이름이 할당된다.
+for (var prop in user) {
+  console.log(prop, user[prop].toUpperCase());
+}
