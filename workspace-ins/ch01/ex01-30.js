e60633d1a4352a2d2571df34ae96b87245f10ef1
kilyong jeong <uzoolove@gmail.com>
2025-10-01 11:48:17 +0900

    기본 함수

--- workspace-ins/ch01/ex01-30.js
+++ workspace-ins/ch01/ex01-30.js
@@ -2,5 +2,17 @@
 기본 함수
 */
 
+console.log('안녕 Function!!!');
+console.log('안녕 Function!!!');
+console.log('안녕 Function!!!');
 
+// 함수 선언
+function sayHello(){
+  console.log('안녕 Function!!!');
+}
+
+// 함수 호출
+sayHello();
+sayHello();
+sayHello();
 
