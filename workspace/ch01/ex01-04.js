3242d583eb5f6d9d8de76e28d65e41ad12589cfb
s2iyeon <s2iyeon.dev@gmail.com>
2025-10-13 09:40:08 +0900

    명령문과 주석

---
+++ workspace/ch01/ex01-04.js
@@ -0,0 +1,15 @@
+/*
+명령문과 주석
+*/
+
+// 일반적인 로그 메시지 출력
+console.log("Hello world!");
+
+// 경고 메시지 출력 (브라우저에서는 노란색으로 표시)
+console.warn("Hello");
+
+// 디버그 메시지 출력
+// console.debug("world!!");
+
+// 에러 메시지 출력 (브라우저에서는 빨간색으로 표시)
+/* console.log("Hello"); */ console.error("world!!");
