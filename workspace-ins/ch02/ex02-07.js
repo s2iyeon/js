4a7d2ff6f34645b33d1775fc1d9390f812f33448
kilyong jeong <uzoolove@gmail.com>
2025-10-13 13:33:31 +0900

    함수 표현식

--- workspace-ins/ch02/ex02-07.js
+++ workspace-ins/ch02/ex02-07.js
@@ -7,6 +7,10 @@
 */
 
 // 함수 표현식(기명 함수)
+const add = function sum(x, y){
+  const result = x + y;
+  return result;
+};
 
 console.log(add(10, 20));
-console.log(sum(10, 20));
+// console.log(sum(10, 20));
