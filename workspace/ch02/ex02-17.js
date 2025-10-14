ccba37f3670dc93a2a8b6d1784aae52c3223c180
s2iyeon <s2iyeon.dev@gmail.com>
2025-10-14 15:14:47 +0900

    함수 내부의 this가 상위 Scope의 this를 참조해야 하지 못하고 함수 자신의 this를 참조하는 경우

---
+++ workspace/ch02/ex02-17.js
@@ -0,0 +1,22 @@
+/*
+* 함수 내부의 this가 상위 Scope의 this를 참조해야 하지 못하고 함수 자신의 this를 참조하는 경우
+*/
+
+var count = 0; // window.count = 0
+const myObj = {
+  count: 0,
+  visit: function(){
+    // 방문자를 한명 증가시킨다.
+    this.count++; // this = myObj
+
+    const visit2 = function(){
+      this.count++; // this = window
+    };
+    
+    visit2(); // this = window
+  },
+};
+
+myObj.visit(); // this = myObj
+myObj.visit(); // this = myObj
+console.log('방문자수', myObj.count); // 2
