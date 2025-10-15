0f757adf1a3932c2906af18b12e95a51f51d8919
kilyong jeong <uzoolove@gmail.com>
2025-10-16 08:41:47 +0900

    3장 추가

---
+++ sample/03/workspace/ch03/ex03-04.js
@@ -0,0 +1,17 @@
+/*
+* 클래스 정의
+
+*/
+
+// HighSchool 생성자 함수
+function HighSchool(kor, eng){
+  
+}
+
+// HighSchool 클래스
+
+
+const h1 = new HighSchool(100, 90);
+console.log('h1', h1.kor, h1.eng); // 100 90
+console.log('h1 총점', h1.sum()); // 190
+console.log('h1 평균', h1.avg()); // 95
