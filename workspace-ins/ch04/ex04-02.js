2a806550a1541118a963608e1b50efac9eeea35a
kilyong jeong <uzoolove@gmail.com>
2025-10-17 14:50:54 +0900

    클로저를 활용한 캡슐화 구현

--- workspace-ins/ch04/ex04-02.js
+++ workspace-ins/ch04/ex04-02.js
@@ -6,11 +6,27 @@
 
 // count 속성과 ride(), getCount() 메서드 작성
 const Counter = function(){
-  
+  let count = 0;
+  this.ride = function(){
+    if(count < 40){
+      count++;
+    }else{
+      console.log('정원 초과!!!');
+    }
+  };
+  this.getCount = function(){
+    return count;
+  };
 };
 
 const c = new Counter();
 c.ride();
 c.ride();
+
+for(let i=0; i<40; i++){
+  c.ride();
+}
+
+// c.count += 40;
 
 console.log('전체 탑승자', c.getCount());
