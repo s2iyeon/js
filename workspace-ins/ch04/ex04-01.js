2b5ec89e84a1f493911852bc569f9fb5fe33a8cc
kilyong jeong <uzoolove@gmail.com>
2025-10-17 14:50:47 +0900

    클로저란?

--- workspace-ins/ch04/ex04-01.js
+++ workspace-ins/ch04/ex04-01.js
@@ -5,3 +5,23 @@
   - 클로저로 인해 유효 범위가 사라진 변수와 함수를 접근할 수 있음
 */
 
+const topLevel = '최상위 변수';
+
+function outerFn(){ // 외부 함수
+  const innerVal = 'outerFn의 지역변수'; // 자유 변수
+  console.log(2, topLevel);
+  console.log(3, innerVal);
+
+  const innerFn = function(){ // 내부 함수
+    console.log(4, innerVal); // outerFn의 지역변수
+  };
+
+  return innerFn;
+}
+
+console.log(1, topLevel);
+// console.log(4, innerVal); // 지역변수는 선언한 함수내에서만 접근 가능
+
+const inner = outerFn();
+
+inner(); // outerFn() 리턴된 후에 호출
