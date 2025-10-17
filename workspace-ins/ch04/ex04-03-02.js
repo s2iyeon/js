41d9e939ae22920fc607fb31673bf46cdaa2582c
kilyong jeong <uzoolove@gmail.com>
2025-10-17 15:22:14 +0900

    커링 - 배열에서 조건에 맞는 요소 찾기

--- workspace-ins/ch04/ex04-03-02.js
+++ workspace-ins/ch04/ex04-03-02.js
@@ -1,5 +1,25 @@
 /*
-* Partial application - 로그 함수
+* 커링 - 배열에서 조건에 맞는 요소 찾기
 
 */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let result;
+
+// 3보다 큰 요소만 추출
+
+console.log('3보다 큰 요소', result); // [4, 5, 6, 7, 8, 9]
+
+// 5보다 큰 요소만 추출
+
+console.log('5보다 큰 요소', result); // [6, 7, 8, 9]
+
+// 6보다 큰 요소만 추출
+
+console.log('6보다 큰 요소', result); // [7, 8, 9]
+
+// 8보다 큰 요소만 추출
+
+console.log('8보다 큰 요소', result); // [9]
+
 
