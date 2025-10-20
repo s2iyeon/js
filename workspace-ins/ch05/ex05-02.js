9898f95222fe191b294af204e393c40ebd1b5df4
kilyong jeong <uzoolove@gmail.com>
2025-10-20 15:02:36 +0900

    + 버튼을 누르면 count 값을 1씩 증가

--- workspace-ins/ch05/ex05-02.js
+++ workspace-ins/ch05/ex05-02.js
@@ -1,3 +1,11 @@
 /*
 * + 버튼을 누르면 count 값을 1씩 증가
 */
+
+const countSpan = document.querySelector('#container > span');
+
+function plus(){
+  const currVal = parseInt(countSpan.lastChild.nodeValue);
+  const nextVal = currVal + 1;
+  countSpan.firstChild.nodeValue = nextVal;
+}
