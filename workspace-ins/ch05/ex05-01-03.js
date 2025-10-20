821a389c55a67ea4b8ba39dbaedec5b72a14d2ab
kilyong jeong <uzoolove@gmail.com>
2025-10-20 15:56:14 +0900

    첫번째와 마지막 구매 항목을 출력. document.querySelector(selector)

--- workspace-ins/ch05/ex05-01-03.js
+++ workspace-ins/ch05/ex05-01-03.js
@@ -6,7 +6,9 @@
 // querySelector를 사용한 CSS 선택자 방식으로 첫번째 요소 선택
 // #purchases: id가 purchases인 요소
 // > li:first-child: purchases의 직계 자식 중 첫번째 li 요소
+console.log('첫번째', document.querySelector('.list > li:first-child').textContent);
 
 // querySelector를 사용한 CSS 선택자 방식으로 마지막 요소 선택
 // #purchases: id가 purchases인 요소
 // > li:last-child: purchases의 직계 자식 중 마지막 li 요소
+console.log('마지막', document.querySelector('.list > li:last-child').textContent);
