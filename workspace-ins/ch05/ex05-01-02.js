f8af563a89e15450b8d9dbef7f3826b12317ec3f
kilyong jeong <uzoolove@gmail.com>
2025-10-20 14:33:13 +0900

    첫번째 구매 항목을 출력

--- workspace-ins/ch05/ex05-01-02.js
+++ workspace-ins/ch05/ex05-01-02.js
@@ -4,10 +4,15 @@
 */
 
 // getElementsByClassName: class 속성으로 요소 선택 (배열 형태로 반환되므로 인덱스 지정 필요)
+const buyList = document.getElementsByClassName('list')[0];
 
 // firstElementChild 사용: 직접 첫 번째 자식 요소 선택
+// const firstLi = buyList.firstElementChild;
+const firstLi = buyList.children[0];
 
 // firstChild를 사용하여 텍스트 노드 선택 (요소 노드의 실제 텍스트 내용)
+const firstTxt = firstLi.firstChild;
+console.log('첫번째 항목', firstTxt.nodeValue);
 
 // lastElementChild를 사용하여 마지막 li 요소의 텍스트 내용 출력
 
