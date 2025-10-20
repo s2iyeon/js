0406f0e3529dc6d2c7b85f24617fd52d92840866
kilyong jeong <uzoolove@gmail.com>
2025-10-20 09:48:00 +0900

    .

--- workspace-ins/ch04/ex04-04-02.js
+++ workspace-ins/ch04/ex04-04-02.js
@@ -21,21 +21,3 @@ consoleWarn('lodash warn message 2');
 const fileError = log.partial('file', 'ERROR');
 fileError('mylib error message 1');
 fileError('mylib error message 2');
-
-class HighSchool{
-  constructor(kor, eng){
-    this.kor = kor;
-    this.eng = eng;
-  }
-  sum(){
-    return this.kor + this.eng;
-  };
-  avg(){
-    return Math.round(this.sum() / 2);
-  };
-}
-
-const h1 = new HighSchool(100, 91);
-console.log('h1', h1.kor, h1.eng); // 100, 91
-console.log('h1 총점', h1.sum()); // 191
-console.log('h1 평균', h1.avg()); // 95.5  <------이 코드에서  avg함수가  kor + eng  평균값을 Math.round 사용해서 반올림하라는거면 96 아닌가여?
