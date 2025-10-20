8e2378fb47932818d095cf6d4b8dffd492a9f132
kilyong jeong <uzoolove@gmail.com>
2025-10-20 10:32:54 +0900

    함수의 내부를 수정하지 않고 메모이제이션 기능 추가

--- workspace-ins/js/mylib.js
+++ workspace-ins/js/mylib.js
@@ -1,5 +1,16 @@
 const mylib = {};
 
+// 모든 함수에 메모이제이션 기능 추가
+Function.prototype.memo = function(key){
+  // 캐시를 위한 코드
+  this._cache = this._cache || {};
+  if(this._cache[key] !== undefined){
+    return this._cache[key];
+  }else{
+    return this._cache[key] = this(key);
+  }
+};
+
 // 함수를 Partial application으로 만들어주는 partial 메서드 추가
 Function.prototype.partial = function(...preArgs){ // [100]
   const fn = this; // sum
