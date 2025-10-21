8110a84125563d3675c4857681aed26534dfe404
kilyong jeong <uzoolove@gmail.com>
2025-10-21 13:41:11 +0900

    .

--- workspace-ins/ch05/ex05-02.js
+++ workspace-ins/ch05/ex05-02.js
@@ -6,16 +6,18 @@ const btn = document.querySelector('#container > button');
 const countSpan = document.querySelector('#container > span');
 
 // + 버튼에 클릭 이벤트 등록
-btn.addEventListener('click', function(){
+btn.addEventListener('click', plus);
+
+function plus(){
   // const currVal = parseInt(countSpan.lastChild.nodeValue);
   // const nextVal = currVal + 1;
   // countSpan.firstChild.nodeValue = nextVal;
   
   // countSpan.textContent = parseInt(countSpan.textContent) + 1;
   countSpan.textContent++;
-});
+}
 
 setTimeout(function(){
   // 이벤트 취소
-  // btn.removeEventListener('click', plus);
+  btn.removeEventListener('click', plus);
 }, 1000*10);
