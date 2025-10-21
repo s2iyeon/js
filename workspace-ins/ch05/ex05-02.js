b69e82533c5be29f9934e780eb91ac8ee05b0041
kilyong jeong <uzoolove@gmail.com>
2025-10-21 09:35:49 +0900

    + 버튼을 누르면 count 값을 1씩 증가

--- workspace-ins/ch05/ex05-02.js
+++ workspace-ins/ch05/ex05-02.js
@@ -6,15 +6,16 @@ const btn = document.querySelector('#container > button');
 const countSpan = document.querySelector('#container > span');
 
 // + 버튼에 클릭 이벤트 등록
-btn.addEventListener('click', plus);
-
-function plus(){
-  const currVal = parseInt(countSpan.lastChild.nodeValue);
-  const nextVal = currVal + 1;
-  countSpan.firstChild.nodeValue = nextVal;
-}
+btn.addEventListener('click', function(){
+  // const currVal = parseInt(countSpan.lastChild.nodeValue);
+  // const nextVal = currVal + 1;
+  // countSpan.firstChild.nodeValue = nextVal;
+  
+  // countSpan.textContent = parseInt(countSpan.textContent) + 1;
+  countSpan.textContent++;
+});
 
 setTimeout(function(){
   // 이벤트 취소
-  btn.removeEventListener('click', plus);
+  // btn.removeEventListener('click', plus);
 }, 1000*10);
