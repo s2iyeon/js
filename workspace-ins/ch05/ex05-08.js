ff8a27d127b5a1f92cfbc61d11d32a511038c5b3
kilyong jeong <uzoolove@gmail.com>
2025-10-22 16:08:23 +0900

    DOM - class 속성 접근

--- workspace-ins/ch05/ex05-08.js
+++ workspace-ins/ch05/ex05-08.js
@@ -4,11 +4,16 @@ console.log(list.classList);
 
 const btn = document.querySelector('button');
 btn.addEventListener('click', function(){
-  list.className = list.className + ' pad100';
+  list.className = 'size30 pad100';
 });
 
 for(let item of list.children){
   item.addEventListener('click', function(){
-    
+    // if(item.classList.contains('line-through')){
+    //   item.classList.remove('line-through');
+    // }else{
+    //   item.classList.add('line-through');
+    // }
+    item.classList.toggle('line-through');
   });
 }
