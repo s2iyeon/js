4ed538885e7c435c009e587c3a69f7bdc6ab42c8
kilyong jeong <uzoolove@gmail.com>
2025-10-28 15:42:10 +0900

    인터페이스로 객체의 타입 선언

--- workspace-ins/ch06/ex06-09.js
+++ workspace-ins/ch06/ex06-09.js
@@ -1,5 +1,9 @@
 // 인터페이스로 객체의 타입 선언
 // ex06-07.ts 복사
 (() => {
+    ;
+    const haru = { name: 'haru', age: 5 };
+    const namu = { name: 'namu', age: 8 };
+    console.log(haru.haru, namu.name.toUpperCase());
 })();
 export {};
