e88898b334e9c5444ab01dafb477d5458049eec6
kilyong jeong <uzoolove@gmail.com>
2025-10-28 14:30:26 +0900

    유니언 타입(union type)

--- workspace-ins/ch06/ex06-05.ts
+++ workspace-ins/ch06/ex06-05.ts
@@ -9,7 +9,23 @@
   function logNumber(msg: number) {
     console.log(msg);
   }
+  // boolean을 받아서 콘솔에 출력하는 함수
+  function logBoolean(msg: boolean) {
+    console.log(msg);
+  }
 
+  logString('hello');
+  logNumber(100);
+  logBoolean(true);
 
+  // 유니언 타입
+  // | 연산자로 여러 타입을 지정
+  function log(msg: string | number | boolean){
+    console.log(msg);
+  }
+
+  log('hello');
+  log(100);
+  log(false);
 
 })();
