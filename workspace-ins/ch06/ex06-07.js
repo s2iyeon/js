db192ad2904a02db972d8686d376c27b510ec33d
kilyong jeong <uzoolove@gmail.com>
2025-10-28 14:50:20 +0900

    타입 별칭으로 객체의 타입 선언

--- workspace-ins/ch06/ex06-07.js
+++ workspace-ins/ch06/ex06-07.js
@@ -1,4 +1,6 @@
 // 타입 별칭으로 객체의 타입 선언
 (() => {
+    const haru = { name: 'haru', age: 5 };
+    const namu = { name: 'namu', age: 8 };
 })();
 export {};
--- workspace-ins/ch06/ex06-07.ts
+++ workspace-ins/ch06/ex06-07.ts
@@ -1,5 +1,12 @@
 // 타입 별칭으로 객체의 타입 선언
 
 (()=>{
-  
+  // 타입 별칭 생성
+  type User = {
+    name: string;
+    age: number;
+  };
+
+  const haru: User = { name: 'haru', age: 5 };
+  const namu: User = { name: 'namu', age: 8 };
 })();
