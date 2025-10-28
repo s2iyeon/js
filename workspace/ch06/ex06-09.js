faeb135b5d3b002fd49ee5b86b455119a321f611
s2iyeon <s2iyeon.dev@gmail.com>
2025-10-28 16:35:05 +0900

    인터페이스로 객체의 타입 선언

---
+++ workspace/ch06/ex06-09.js
@@ -0,0 +1,9 @@
+// 인터페이스로 객체의 타입 선언
+// ex06-07.ts 복사
+(() => {
+    ;
+    const haru = { name: 'haru', age: 5 };
+    const namu = { name: 'namu', age: 8 };
+    console.log(haru.age, namu.name.toUpperCase());
+})();
+export {};
