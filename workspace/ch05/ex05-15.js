7f93ed164ebff367a07df64ac5364acd2e364100
s2iyeon <s2iyeon.dev@gmail.com>
2025-10-28 09:03:49 +0900

    Geolocation API

--- workspace/ch05/ex05-15.js
+++ workspace/ch05/ex05-15.js
@@ -1,3 +1,4 @@
+console.log ("test"); 
 const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 const options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
