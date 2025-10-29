cb7c4172cad27f748cac57c6759a17ea86553950
kilyong jeong <uzoolove@gmail.com>
2025-10-29 14:33:22 +0900

    echo 함수 - 제네릭 타입 제약

--- workspace-ins/ch06/ex06-17-03.js
+++ workspace-ins/ch06/ex06-17-03.js
@@ -1,5 +1,26 @@
 // echo 함수 - 제네릭 타입 제약
 // ex06-17-02.ts 복사
 (() => {
+    function echo(msg) {
+        return msg;
+    }
+    console.log(echo('hello'));
+    console.log(echo(100));
+    // console.log(echo<boolean>(true));
+    // console.log(echo<Date>(new Date()));
+    const str = echo('hello');
+    const num = echo(123.456);
+    console.log(str.toUpperCase(), num.toFixed(2));
+    function echo2(msg) {
+        console.log(msg.length);
+    }
+    echo2('haru');
+    echo2([1, 2, 3]);
+    const naverHire = {
+        length: 50000,
+        domain: '프론트엔드',
+        techStack: 'JS, TS, React, Next.js'
+    };
+    echo2(naverHire);
 })();
 export {};
--- workspace-ins/ch06/ex06-17-03.ts
+++ workspace-ins/ch06/ex06-17-03.ts
@@ -2,5 +2,38 @@
 // ex06-17-02.ts 복사
 
 (()=>{
-  
+  function echo<T extends string | number>(msg: T): T {
+    return msg;
+  }
+
+  console.log(echo<string>('hello'));
+  console.log(echo<number>(100));
+  // console.log(echo<boolean>(true));
+  // console.log(echo<Date>(new Date()));
+
+  const str = echo<string>('hello');
+  const num = echo<number>(123.456);
+
+  console.log(str.toUpperCase(), num.toFixed(2));
+
+  function echo2<T extends { length: number }>(msg: T): void {
+    console.log(msg.length);
+  }
+
+  echo2<string>('haru');
+  echo2<number[]>([1, 2, 3]);
+
+  interface Hire {
+    length: number;
+    domain: string;
+    techStack: string;
+  }
+
+  const naverHire: Hire = {
+    length: 50000,
+    domain: '프론트엔드',
+    techStack: 'JS, TS, React, Next.js'
+  };
+
+  echo2<Hire>(naverHire);
 })();
