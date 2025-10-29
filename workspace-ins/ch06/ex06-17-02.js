e92988a006820d9c47c5b2899365e043b69e1962
kilyong jeong <uzoolove@gmail.com>
2025-10-29 14:33:16 +0900

    echo 함수 - 제네릭 함수

--- workspace-ins/ch06/ex06-17-02.js
+++ workspace-ins/ch06/ex06-17-02.js
@@ -1,5 +1,12 @@
 // echo 함수 - 제네릭 함수
 // ex06-17.ts 복사
 (() => {
+    function echo(msg) {
+        return msg;
+    }
+    console.log(echo('hello'));
+    console.log(echo(100));
+    console.log(echo(true));
+    console.log(echo(new Date()));
 })();
 export {};
--- workspace-ins/ch06/ex06-17-02.ts
+++ workspace-ins/ch06/ex06-17-02.ts
@@ -2,5 +2,12 @@
 // ex06-17.ts 복사
 
 (()=>{
-  
+  function echo<T>(msg: T): T {
+    return msg;
+  }
+
+  console.log(echo<string>('hello'));
+  console.log(echo<number>(100));
+  console.log(echo<boolean>(true));
+  console.log(echo<Date>(new Date()));
 })();
