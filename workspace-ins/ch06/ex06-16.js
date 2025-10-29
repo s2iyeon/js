66b0dc1c17160fb2b14a24db8de5af757026d9a8
kilyong jeong <uzoolove@gmail.com>
2025-10-29 13:09:50 +0900

    인터페이스 선언 병합

--- workspace-ins/ch06/ex06-16.js
+++ workspace-ins/ch06/ex06-16.js
@@ -1,5 +1,12 @@
 // 인터페이스 선언 병합
 // ex06-15.ts 복사
 (() => {
+    const todo = {
+        id: 1,
+        title: '인터페이스 병합',
+        content: '인터페이스 병합을 해봐요',
+        done: true,
+    };
+    console.log(todo);
 })();
 export {};
--- workspace-ins/ch06/ex06-16.ts
+++ workspace-ins/ch06/ex06-16.ts
@@ -2,5 +2,24 @@
 // ex06-15.ts 복사
 
 (()=>{
-  
+  interface Todo {
+    id: number;
+    title: string;
+    content: string;
+  }
+
+  interface Todo {
+    // id: string; // 동일한 속성을 지정하려면 type이 같아야 함
+    done: boolean;
+    title: string;
+  }
+
+  const todo: Todo = {
+    id: 1,
+    title: '인터페이스 병합',
+    content: '인터페이스 병합을 해봐요',
+    done: true,
+  };
+
+  console.log(todo);
 })();
