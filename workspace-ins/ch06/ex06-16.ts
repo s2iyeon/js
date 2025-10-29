d6ae1b7e7c19c4786c54aac5772a664acb844c1d
kilyong jeong <uzoolove@gmail.com>
2025-10-29 13:29:12 +0900

    인덱스 시그니처 추가

--- workspace-ins/ch06/ex06-16.ts
+++ workspace-ins/ch06/ex06-16.ts
@@ -1,4 +1,4 @@
-// 인터페이스 선언 병합
+// 인터페이스 선언 병합, 인덱스 시그니처
 // ex06-15.ts 복사
 
 (()=>{
@@ -21,5 +21,39 @@
     done: true,
   };
 
-  console.log(todo);
+  const todo2: Todo = {
+    id: 1,
+    title: '인터페이스 병합',
+    content: '인터페이스 병합을 해봐요',
+    done: true,
+  };
+
+  console.log(todo, todo2);
+
+
+  interface User {
+    name: string;
+    email: string;
+    // phone?: string;
+    // address?: string;
+    // job?: string;
+
+    [key: string]: string; // 인덱스 시그니처
+  }
+
+  const haru: User = {
+    name: '하루',
+    email: 'haru@gmail.com',
+    phone: '01022223333',
+  };
+  const namu: User = {
+    name: '나무',
+    email: 'namu@gmail.com',
+    address: '경기도 김포시',
+    job: '강아지',
+    gender: '여자'
+  };
+
+  console.log(haru, namu);
+
 })();
