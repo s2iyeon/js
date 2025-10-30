800f88b7bc90303f9e8aa2df5080afda886a724b
kilyong jeong <uzoolove@gmail.com>
2025-10-30 14:29:19 +0900

    유틸리티 타입 - Omit

--- workspace-ins/ch06/ex06-33.ts
+++ workspace-ins/ch06/ex06-33.ts
@@ -1,5 +1,30 @@
 // 유틸리티 타입 - Omit
 
 (()=>{
-  
+  interface Todo {
+    id: number;
+    title: string;
+    content: string;
+    done: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  // type TodoInfo = {
+  //   id: number;
+  //   title: string;
+  //   content: string;
+  //   done: boolean;
+  // }
+
+  type TodoInfo = Omit<Todo, 'createdAt' | 'updatedAt'>;
+
+  const todo: TodoInfo = {
+    id: 1,
+    title: '제목1',
+    content: '내용1',
+    done: false
+  };
+
+  console.log(todo);
 })();
