3d39ec17ff89f4708206a99789fd60263eeb1022
kilyong jeong <uzoolove@gmail.com>
2025-10-30 15:46:08 +0900

    맵드 타입 - 유틸리티 타입 구현 원리

--- workspace-ins/ch06/ex06-37.js
+++ workspace-ins/ch06/ex06-37.js
@@ -1,4 +1,18 @@
 // 맵드 타입 - 유틸리티 타입 구현 원리
 (() => {
+    const todo1 = {
+        title: '제목1',
+    };
+    const todo2 = {
+        title: '제목2',
+    };
+    const todo3 = {
+        title: '제목3',
+    };
+    // todo3.title = '수정'; // 컴파일 에러
+    const todo4 = {
+        title: '제목4',
+    };
+    console.log(todo1, todo2);
 })();
 export {};
--- workspace-ins/ch06/ex06-37.ts
+++ workspace-ins/ch06/ex06-37.ts
@@ -1,5 +1,40 @@
 // 맵드 타입 - 유틸리티 타입 구현 원리
 
 (()=>{
+  interface Todo {
+    id: string;
+    title: string;
+    content: string;
+  }
+
+  // keyof T = 'id' | 'title' | 'content'
+  // T[P] -> Todo['id'] -> string
+  type MyPartial<T> = {
+    [P in keyof T]?: T[P];
+  };
+
+  const todo1: MyPartial<Todo> = {
+    title: '제목1',
+  };
+  const todo2: Partial<Todo> = {
+    title: '제목2',
+  };
+
+
+
+  // 읽기 전용으로 바뀐 타입 반환
+  type MyReadonly<T> = {
+    readonly [P in keyof T]: T[P];
+  };
   
+  const todo3: MyReadonly<Partial<Todo>> = {
+    title: '제목3',
+  };
+  // todo3.title = '수정'; // 컴파일 에러
+
+  const todo4: Pick<MyReadonly<Partial<Todo>>, 'title'> = {
+    title: '제목4',
+  };
+
+  console.log(todo1, todo2);
 })();
