05ee29c7f849ab65575302bdde4a1af0d0a5efa0
kilyong jeong <uzoolove@gmail.com>
2025-10-30 16:08:01 +0900

    고급 타입 연산자 - keyof

--- workspace-ins/ch06/ex06-38.ts
+++ workspace-ins/ch06/ex06-38.ts
@@ -5,19 +5,23 @@
     id: string;
     title: string;
     content: string;
+    done: boolean;
   }
 
   // 지정한 객체의 속성값을 반환
-  function getProperty(){
-
+  // function getProperty(obj: Todo, key: 'id' | 'title' | 'content' | 'done') {
+  function getProperty(obj: Todo, key: keyof Todo) {
+    return obj[key];
   }
 
   const todo: Todo = {
     id: '1',
     title: '제목1',
-    content: '내용1'
+    content: '내용1',
+    done: false,
   };
 
   console.log(getProperty(todo, 'title')); // 제목1
   console.log(getProperty(todo, 'content')); // 내용1
+  console.log(getProperty(todo, 'done')); // false
 })();
