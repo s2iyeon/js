9be0ff8480643094dc33b4e6854f950786e15f53
kilyong jeong <uzoolove@gmail.com>
2025-10-30 15:10:54 +0900

    맵드 타입 - 기본 문법

--- workspace-ins/ch06/ex06-35.ts
+++ workspace-ins/ch06/ex06-35.ts
@@ -1,5 +1,24 @@
 // 맵드 타입 - 기본 문법
 
 (()=>{
-  
+  // type User = {
+  //   id: string;
+  //   name: string;
+  //   addr: string;
+  //   phone: string;
+  // };
+
+  type UserFields = 'id' | 'name' | 'addr' | 'phone';
+  type User = {
+    [Prop in UserFields]: string;
+  };
+
+  const haru: User = {
+    id: '1',
+    name: '하루',
+    addr: '김포시',
+    phone: '01022223333'
+  };
+
+  console.log(haru);
 })();
