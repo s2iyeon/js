37d7385aecc912c6ce76dbea753dbeb27842c9b1
kilyong jeong <uzoolove@gmail.com>
2025-10-30 10:44:38 +0900

    타입 단언 - JS로 작성된 코드를 TS로 리팩토링. 로직은 수정하지 말고 타입만 추가 정의

--- workspace-ins/ch06/ex06-23-02.js
+++ workspace-ins/ch06/ex06-23-02.js
@@ -1,5 +1,19 @@
 // FIXME: 타입 단언 - JS로 작성된 코드를 TS로 리팩토링. 로직은 수정하지 말고 타입만 추가 정의
 // ex06-23.js 복사
 (() => {
+    const todo1 = {
+        title: 'Ajax',
+        content: '서버와 통신하는 Ajax 배우기'
+    };
+    const todo2 = {
+        title: '기타',
+        content: 'try-catch로 에러 처리 배우기'
+    };
+    const todo3 = {};
+    todo3.title = '내장 함수';
+    const todo4 = { title: '내장 객체' };
+    todo4.content = 'Date, String, Array 등의 내장 객체 배우기';
+    // todo4.content가 없으면 런타임 에러
+    console.log(todo1, todo2, todo3, todo4.content.toUpperCase());
 })();
 export {};
