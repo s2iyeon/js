311a659b044af3c77f10076eac90e6983c3f8b2e
kilyong jeong <uzoolove@gmail.com>
2025-10-30 11:50:31 +0900

    // 타입 가드 함수 작성

--- workspace-ins/ch06/ex06-27.js
+++ workspace-ins/ch06/ex06-27.js
@@ -1,5 +1,40 @@
 // 타입 가드 함수 작성
 // ex06-26.ts 복사
 (() => {
+    const user1 = {
+        name: '하루',
+        age: 5,
+        admin: false,
+    };
+    const user2 = {
+        name: '나무',
+        admin: true,
+        level: 2
+    };
+    const user3 = {
+        name: '게스트'
+    };
+    helloUser(user1);
+    helloUser(user2);
+    // User 타입이나 AdminUser 타입을 받아서 각각의 타입에 맞는 코드를 실행하는 함수
+    function helloUser(user) {
+        if (isAdmin(user)) { // AdminUser
+            console.log(`안녕하세요. 레벨 ${user.level} ${user.name} 관리자님.`);
+        }
+        else if (isGuest(user)) { // Guest
+            console.log(`안녕하세요. ${user.name} 님.`);
+        }
+        else { // User
+            console.log(`안녕하세요. ${user.age}살 ${user.name} 회원님.`);
+        }
+    }
+    // 타입 가드 구문이 복잡할 경우나 재사용 해야될 경우 타입 가드 함수를 작성
+    // user is AdminUser의 의미는 isAdmin() 함수가 true를 리턴할 경우 user의 타입이 AdminUser가 됨
+    function isAdmin(user) {
+        return 'admin' in user && user.admin === true;
+    }
+    function isGuest(user) {
+        return !('age' in user) && !('level' in user) && user.name === '게스트';
+    }
 })();
 export {};
