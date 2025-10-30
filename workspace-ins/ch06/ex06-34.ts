fc85c84008ff3bf8a545a001c0e82182060e66c1
kilyong jeong <uzoolove@gmail.com>
2025-10-30 14:43:29 +0900

    유틸리티 타입 - Record

--- workspace-ins/ch06/ex06-34.ts
+++ workspace-ins/ch06/ex06-34.ts
@@ -1,5 +1,23 @@
 // 유틸리티 타입 - Record
 
 (()=>{
-  
+  // type UserPermissions = {
+  //   admin: string[];
+  //   user: string[];
+  //   guest: string[];
+  //   // level1: string[];
+  //   // level2: string[];
+  //   // level3: string[];
+  // };
+
+  // type UserRole = 'admin' | 'user' | 'guest';
+  type UserPermissions = Record<'admin' | 'user' | 'guest', string[]>;
+
+  const freeBoardPermissions: UserPermissions = {
+    admin: ['read', 'write', 'delete'],
+    user: ['read', 'write'],
+    guest: ['read'],
+  };
+
+  console.log('게스트 권한', freeBoardPermissions.guest);
 })();
