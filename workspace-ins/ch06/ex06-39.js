3810b453b0208af14df06bf55c0bc69038ea5b39
kilyong jeong <uzoolove@gmail.com>
2025-10-30 16:12:09 +0900

    고급 타입 연산자 - typeof

--- workspace-ins/ch06/ex06-39.js
+++ workspace-ins/ch06/ex06-39.js
@@ -1,4 +1,15 @@
 // 고급 타입 연산자 - typeof
 (() => {
+    const haru = {
+        name: '하루',
+        age: 5,
+        city: '김포시'
+    };
+    const namu = {
+        name: '나무',
+        age: 8,
+        city: '김포시'
+    };
+    console.log(haru, namu);
 })();
 export {};
