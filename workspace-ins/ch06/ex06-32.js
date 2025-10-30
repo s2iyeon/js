33f9a664e09d82b4d445aadc2d6a19764bae6fee
kilyong jeong <uzoolove@gmail.com>
2025-10-30 14:29:14 +0900

    유틸리티 타입 - Pick

--- workspace-ins/ch06/ex06-32.js
+++ workspace-ins/ch06/ex06-32.js
@@ -1,4 +1,11 @@
 // 유틸리티 타입 - Pick
 (() => {
+    // { title: string, content: string } 타입으로 지정
+    const todo = {
+        title: '제목1',
+        content: '내용1'
+    };
+    console.log(todo.title, todo.content); // 제목1 내용1
+    // console.log(todo.done); // 컴파일 에러
 })();
 export {};
--- workspace-ins/ch06/ex06-32.ts
+++ workspace-ins/ch06/ex06-32.ts
@@ -1,5 +1,28 @@
 // 유틸리티 타입 - Pick
 
 (()=>{
-  
+  interface Todo {
+    id: number;
+    title: string;
+    content: string;
+    done: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  // type TodoRegist = {
+  //   title: string;
+  //   content: string;
+  // }
+
+  type TodoRegist = Pick<Todo, 'title' | 'content'>;
+
+  // { title: string, content: string } 타입으로 지정
+  const todo: TodoRegist = {
+    title: '제목1',
+    content: '내용1'
+  };
+
+  console.log(todo.title, todo.content); // 제목1 내용1
+  // console.log(todo.done); // 컴파일 에러
 })();
