0bf98b70380e155e17be094405ee7c4f66fdd9e9
kilyong jeong <uzoolove@gmail.com>
2025-10-30 14:28:56 +0900

    유틸리티 타입 - Required

--- workspace-ins/ch06/ex06-30.ts
+++ workspace-ins/ch06/ex06-30.ts
@@ -1,5 +1,31 @@
 // 유틸리티 타입 - Required
 
 (()=>{
-  
+  interface Todo {
+    title: string;
+    content?: string;
+    done?: boolean;
+  }
+
+  // interface RequiredTodo {
+  //   title: string;
+  //   content: string;
+  //   done: boolean;
+  // }
+// 
+  const todo1: Todo = {
+    title: '할일1',
+    // content: '내용1',
+    // done: false,
+  };
+
+  // const todo2: RequiredTodo = {
+  const todo2: Required<Todo> = {
+    title: '할일2',
+    content: '내용2',
+    done: true,
+  };
+
+  console.log(todo1);
+  console.log(todo2);
 })();
