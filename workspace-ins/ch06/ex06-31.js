b9fcb4909d5cab763c33278964af934dcee1c559
kilyong jeong <uzoolove@gmail.com>
2025-10-30 14:29:04 +0900

    유틸리티 타입 - Partial

--- workspace-ins/ch06/ex06-31.js
+++ workspace-ins/ch06/ex06-31.js
@@ -1,4 +1,15 @@
 // 유틸리티 타입 - Partial
 (() => {
+    const todo1 = {
+        title: '할일1',
+        content: '내용1',
+        done: false,
+    };
+    // const todo2: OptionalTodo = {
+    const todo2 = {
+        title: '할일2',
+    };
+    console.log(todo1);
+    console.log(todo2);
 })();
 export {};
--- workspace-ins/ch06/ex06-31.ts
+++ workspace-ins/ch06/ex06-31.ts
@@ -1,5 +1,29 @@
 // 유틸리티 타입 - Partial
 
 (()=>{
-  
+  interface Todo {
+    title: string;
+    content: string;
+    done: boolean;
+  }
+
+  interface OptionalTodo {
+    title?: string;
+    content?: string;
+    done?: boolean;
+  }
+
+  const todo1: Todo = {
+    title: '할일1',
+    content: '내용1',
+    done: false,
+  };
+
+  // const todo2: OptionalTodo = {
+  const todo2: Partial<Todo> = {
+    title: '할일2',
+  };
+
+  console.log(todo1);
+  console.log(todo2);
 })();
