7ba7398018227b0f48557ca8c5cbd99ee2798945
kilyong jeong <uzoolove@gmail.com>
2025-10-31 17:32:33 +0900

    내장 객체 Array - concat()

--- workspace-ins/ch07/ex07-14.ts
+++ workspace-ins/ch07/ex07-14.ts
@@ -1,14 +1,17 @@
-// 내장 객체 Array - includes(), concat()
+// 내장 객체 Array - concat()
 
 (()=>{
 
   const arr = ['오렌지', '딸기', '레몬'];
-  console.log(arr.includes('레몬')); // true
-  console.log(arr.includes('사과')); // false
 
   // 배열 결합
-  const arr2 = arr.concat(['사과', '바나나'], ['포도']);
-  console.log(arr2.includes('사과')); // true
+  // const arr2 = arr.concat(['사과', '바나나'], ['포도']);
+
+  const newArr1 = ['사과', '바나나'];
+  const newArr2 = ['포도'];
+
+  // 전개 구문
+  const arr2 = [ ...arr, ...newArr1, ...newArr2 ];
 
   console.log(arr); // [ '오렌지', '딸기', '레몬' ]
   console.log(arr2); // [ '오렌지', '딸기', '레몬', '사과', '바나나', '포도' ]
