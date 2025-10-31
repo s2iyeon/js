f2a353432855d645d98cf2d1b27828cb3a81ea9f
kilyong jeong <uzoolove@gmail.com>
2025-10-31 17:53:00 +0900

    내장 객체 Array - reduce()

--- workspace-ins/ch07/ex07-19.ts
+++ workspace-ins/ch07/ex07-19.ts
@@ -1,5 +1,12 @@
 // 내장 객체 Array - reduce()
 
 (()=>{
-  
+  const arr = [1, 2, 3, 4];
+  const initialValue = 0;
+  const sum = arr.reduce(function(acc, num, index){
+    console.log(`${index} -> ${acc} + ${num} = ${acc + num}`);
+    return acc + num;
+  }, initialValue);
+
+  console.log(sum); // 0 + 1 + 2 + 3 + 4 = 10
 })();
