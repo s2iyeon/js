07a1ee3a5d8bf87216efc7fcd0a23657295c39a1
kilyong jeong <uzoolove@gmail.com>
2025-10-31 17:32:46 +0900

    내장 객체 Array - forEach(), map()

--- workspace-ins/ch07/ex07-16.js
+++ workspace-ins/ch07/ex07-16.js
@@ -1,13 +1,23 @@
 // 내장 객체 Array - forEach(), map()
 (() => {
     const arr = [10, 20, 30];
-    // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(for문)
+    // arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(for문)
     let newArr = [];
+    for (let elem of arr) {
+        newArr.push(elem * elem);
+    }
     console.log('for문', newArr);
-    // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(forEach)
+    // arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(forEach)
     newArr = [];
+    arr.forEach(elem => {
+        newArr.push(elem * elem);
+    });
     console.log('forEach', newArr); // [100, 400, 900]
-    // FIXME arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(map)
+    // arr의 각 요소를 제곱하여 새로운 배열을 생성하는 코드를 작성하세요.(map)
+    newArr = arr.map(elem => {
+        return elem * elem;
+    });
+    newArr = arr.map(elem => elem * elem);
     console.log('map', newArr); // [100, 400, 900]
 })();
 export {};
