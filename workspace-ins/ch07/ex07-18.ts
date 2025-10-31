b4b85171c37774af7752f81a0dd2b6f936f446b9
kilyong jeong <uzoolove@gmail.com>
2025-10-31 17:52:52 +0900

    내장 객체 Array - some(), every()

--- workspace-ins/ch07/ex07-18.ts
+++ workspace-ins/ch07/ex07-18.ts
@@ -1,13 +1,17 @@
-// // 내장 객체 Array - some(), every()
+// 내장 객체 Array - some(), every()
 
-// (()=>{
+(()=>{
   
-//   const arr = [1, 2, 3];
-//   // 2의 배수가 하나라도 있나?
-//   const hasEven = arr.some(); // true
-//   // 전부 2의 배수인가?
-//   const allEven = arr.every(); // false
+  // const arr = [1, 2, 3];
+  // const arr = [2, 4, 6];
+  const arr = [1, 3, 5];
 
-//   console.log(hasEven, allEven);
+  // 2의 배수가 하나라도 있나?
+  const hasEven = arr.some(elem => elem % 2 === 0); // true
 
-// })();
+  // 전부 2의 배수인가?
+  const allEven = arr.every(elem => elem % 2 === 0); // false
+
+  console.log(hasEven, allEven);
+
+})();
