0abd2d880622e49b45ce3a31a6f661d6e7887e55
kilyong jeong <uzoolove@gmail.com>
2025-11-03 13:11:14 +0900

    에러 핸들링 - throw 문으로 에러 전달

--- workspace-ins/ch08/ex08-04.ts
+++ workspace-ins/ch08/ex08-04.ts
@@ -10,12 +10,21 @@
    * @throws 0으로 나눌 수 없습니다.
    */
   function divide(x: number, y: number) {
+    if(y === 0){
+      throw new Error('0으로 나눌 수 없습니다.');
+    }
     return x / y;
   }
 
   function f1() {
-    const result1 = divide(10, Math.round(Math.random()));
-    console.log(result1);
+    try{
+      const result1 = divide(10, Math.round(Math.random()));
+      console.log(result1);
+    }catch(err){
+      if(err instanceof Error){
+        console.error(err.message);
+      }
+    }
     
     const result2 = divide(10, 2);
     console.log(result2);
