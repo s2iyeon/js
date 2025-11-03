622c71b57b6041f7e52fb5f1efbbc21090587b14
kilyong jeong <uzoolove@gmail.com>
2025-11-03 17:27:18 +0900

    비동기 함수 - Promise 사용(에러 처리 추가)

--- workspace-ins/ch08/ex08-10-02.js
+++ workspace-ins/ch08/ex08-10-02.js
@@ -4,5 +4,36 @@
 // 에러 처리 추가
 // ex08-10.ts 복사
 (() => {
+    function f1() {
+        return new Promise((resolve, reject) => {
+            console.log('3. f1 호출됨.');
+            const delay = Math.floor(Math.random() * 1000 * 10);
+            console.log(`4. 작업 시작...`);
+            setTimeout(() => {
+                // 시간 걸리는 작업 처리중
+                // ...
+                if (delay < 5000) {
+                    resolve('f1의 작업 결과: ' + delay);
+                }
+                else {
+                    reject('f1의 작업 실패 사유: ' + delay);
+                }
+            }, delay);
+            console.log('5. f1 리턴됨.');
+        });
+    }
+    function test() {
+        console.log('2. test 호출됨.');
+        f1().then((result) => {
+            console.log(result);
+        }).catch((reason) => {
+            console.error(reason);
+        });
+        console.log('6. test 리턴됨');
+    }
+    // 프로그램 실행 흐름에 맞춰서 콘솔 출력 메세지 앞에 번호 추가
+    console.log('1. 작업 시작.');
+    test();
+    console.log('7. 작업 종료.');
 })();
 export {};
