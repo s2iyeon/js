9ed8ce3cd44110a1be51df2b5e281f7c9693e22a
s2iyeon <s2iyeon.dev@gmail.com>
2025-11-04 09:13:59 +0900

    일반 함수(동기 방식)

---
+++ workspace/ch08/ex08-06-02.js
@@ -0,0 +1,9 @@
+// 일반 함수(동기 방식)
+"use strict";
+function test(a, b) {
+  console.log(this); // window
+  x = 10; // window.x = 10;
+  console.log(x); // 10
+  console.log(a + a); // 40
+}
+test(10, 20);
