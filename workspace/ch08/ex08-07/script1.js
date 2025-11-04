6dbe85c0e0520af667b1a35e0b8c9d02d909f376
s2iyeon <s2iyeon.dev@gmail.com>
2025-11-04 09:19:04 +0900

    async, defer

---
+++ workspace/ch08/ex08-07/script1.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('script-1.js 실행');
+  const btn1 = document.querySelector('#btn1');
+  btn1.addEventListener('click', () => {
+    console.log('버튼1 클릭');
+  });
+});
