3b4cdfde14c6bddd58d30ce0e8da21a66177c6bc
s2iyeon <s2iyeon.dev@gmail.com>
2025-11-04 09:11:40 +0900

    에러 핸들링 - try...catch 문으로 에러 처리

---
+++ workspace/ch08/ex08-03-02.js
@@ -0,0 +1,21 @@
+// 에러 핸들링 - try...catch 문으로 에러 처리 2
+// ex08-02.ts 복사
+(() => {
+    function f1() {
+        const fn = new Function('retrun 10;'); // SyntaxError
+        fn();
+        console.log('1. f1 함수 정상 종료');
+    }
+    function f2() {
+        try {
+            f1(); // SyntaxError
+        }
+        catch (err) {
+            console.error(err);
+        }
+        console.log('2. f2 함수 정상 종료');
+    }
+    f2();
+    console.log('3. 프로그램 정상 종료');
+})();
+export {};
