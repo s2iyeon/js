edf5751e3da7356cfa3b8ca323b23814e324d653
s2iyeon <s2iyeon.dev@gmail.com>
2025-11-05 17:36:38 +0900

    랜덤 고양이 이미지 가져오기 - Fetch API 사용

---
+++ workspace/ch09/ex09-02.js
@@ -0,0 +1,24 @@
+// 랜덤 고양이 이미지 가져오기 - Fetch API 사용
+// ex09-01.ts 복사
+import { appendImages } from "./utils.js";
+const url = 'https://api.thecatapi.com/v1/images/search';
+const btn = document.querySelector('button');
+btn?.addEventListener('click', getImages);
+async function getImages() {
+    try {
+        const response = await fetch(url);
+        console.log('response', response);
+        if (response.ok) { // 응답 상태 코드가 2xx 일 경우
+            const data = await response.json();
+            console.log('data', data);
+            appendImages(data);
+        }
+        else { // 응답 상태 코드가 4xx, 5xx 일 경우
+            console.error(response.status, '에러');
+        }
+    }
+    catch (err) {
+        console.error('네트워크 에러', err);
+    }
+}
+;
