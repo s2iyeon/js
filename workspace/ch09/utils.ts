29fd397f6695a54097f687c65318ebfb5f85f7e7
s2iyeon <s2iyeon.dev@gmail.com>
2025-11-05 17:35:03 +0900

    랜덤 고양이 이미지 가져오기 - XMLHttpRequest 사용

---
+++ workspace/ch09/utils.ts
@@ -0,0 +1,17 @@
+import type { Cat } from "./types.js";
+
+// 이미지 목록을 받아서 화면에 추가
+export function appendImages(images: Cat[]){
+  const liList = images.map((item) => {
+    const li = document.createElement('li'); // <li>
+    const img = document.createElement('img'); // <img>
+    img.src = item.url; // <img src="https://sdfsdf.sdf">
+    img.height = 200; // <img src="https://sdfsdf.sdf" height="200">
+    img.alt = '고양이'; // <img src="https://sdfsdf.sdf" height="200" alt="고양이">
+    li.appendChild(img); // <li><img src="https://sdfsdf.sdf" height="200" alt="고양이"></li>
+    return li;
+  });
+
+  const catList = document.querySelector('#cat-list');
+  catList?.prepend(...liList); // <ul><li><img src="https://sdfsdf.sdf" height="200" alt="고양이"></li>...</ul>
+}
